f82eb80ee819593f8f4f1daea884a3577dde7488
Dhawaldeep Gaur <dhawalgaur17@gmail.com>
2023-07-10 00:25:23 +0530

    Full screen size

--- lesson-1-dev-env-setup/src/main.ts
+++ lesson-1-dev-env-setup/src/main.ts
@@ -15,12 +15,12 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 // Camera
-const size = {
-    width: 800,
-    height: 600,
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
 };
 
-const camera = new THREE.PerspectiveCamera(75, size.width/size.height);
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
@@ -28,6 +28,35 @@ scene.add(camera);
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
 });
-renderer.setSize(size.width, size.height);
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 renderer.render(scene, camera);
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Animate
+ */
+const tick = () => {
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+}
+
+tick();
